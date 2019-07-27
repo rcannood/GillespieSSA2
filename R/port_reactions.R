80da181e618497fcbf53235dddccf3286adb0c6b
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-27 08:33:12 +0200

    add x0 parameter since the names are needed

--- R/port_reactions.R
+++ R/port_reactions.R
@@ -3,16 +3,18 @@
 #' This is a helper function to tranform GillesieSSA-style
 #' paramters to GillespieSSA2.
 #'
+#' @param x0 The `x0` parameter of [GillespieSSA::ssa()].
 #' @param a The `a` parameter of [GillespieSSA::ssa()].
 #' @param nu The `nu` parameter of [GillespieSSA::ssa()].
 #'
 #' @export
 #'
 #' @examples
+#' x0  <- c(Y1 = 1000, Y2 = 1000)
 #' a   <- c("c1*Y1","c2*Y1*Y2","c3*Y2")
 #' nu  <- matrix(c(+1,-1,0,0,+1,-1),nrow=2,byrow=TRUE)
-#' port_reactions(a, nu)
-port_reactions <- function(a, nu) {
+#' port_reactions(x0, a, nu)
+port_reactions <- function(x0, a, nu) {
   map(
     seq_along(a),
     function(i) {
